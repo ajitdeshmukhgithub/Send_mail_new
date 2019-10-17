7b06d33a915e2a9dd1dc5548edf3d637ec863093
Ajits <Ajits@AJIT-PC>
2019-10-18 01:01:04 +0530

    first commit

---
+++ Send_Mail_Auto/src/main/java/com/javaSource/Send_Mail_Auto/Testing.java
@@ -0,0 +1,24 @@
+package com.javaSource.Send_Mail_Auto;
+
+import java.util.TimerTask;
+import com.javaConstants.Constants;
+
+public class Testing extends TimerTask
+{
+
+	public void run()
+	{
+
+                GMailServer sender = new GMailServer(Constants.setFrom, Constants.setPassword);
+
+	            try {
+			    sender.sendMail("Subject","This is Java4s",Constants.setFrom,Constants.emailTO);
+			}
+                   catch (Exception e) {
+			     e.printStackTrace();
+			}  
+
+				System.out.println("Email Sent Succesfully...");
+
+	        }
+}
